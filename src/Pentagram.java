0653412e1852d73bb7eebd21455a9d73314467c7
Idra <percy.baker.0@gmail.com>
2023-10-27 00:42:53 +0100

    intellij really???

--- src/Pentagram.java
+++
@@ -1,7 +0,0 @@
-public final class Pentagram {
-    private Pentagram() {}
-
-    public static void summonDevil() {
-        System.out.println("Hello! MyEngagement here, how can I help?");
-    }
-}
