93d5354c659058f006464581dc89ca0edfaadcd5
Idra <percy.baker.0@gmail.com>
2023-10-27 00:43:05 +0100

    REALLY??

---
+++ real_src/Pentagram.java
@@ -0,0 +1,7 @@
+public final class Pentagram {
+    private Pentagram() {}
+
+    public static void summonDevil() {
+        System.out.println("Hello! MyEngasgement here, how can I help?");
+    }
+}
